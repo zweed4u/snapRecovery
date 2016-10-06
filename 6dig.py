472621e6985df81de5192891443c14e75a5f2733
ZACHARY WEEDEN (RIT Student) <zdw7287@ad.rit.edu>
2016-10-06 15:32:12 -0400

    fixing corrupted paste

--- 6dig.py
+++ 6dig.py
@@ -6,9 +6,9 @@ sel=[0,1,2,3,4,5,6,7,8,9]
 lstTup=[p for p in itertools.product(sel, repeat=6)]
 for tup in lstTup:
         choice=tup
-        formatted=str(tup[0])+str(tup[1])+str(tup[2])+str(tup[3])+str(tup[4])+s$
+        formatted=str(tup[0])+str(tup[1])+str(tup[2])+str(tup[3])+str(tup[4])+str(tup[5])
 print formatted
 '''
 choice=random.choice([p for p in itertools.product(sel, repeat=6)])
-formatted=str(choice[0])+str(choice[1])+str(choice[2])+str(choice[3])+str(choic$
+formatted=str(choice[0])+str(choice[1])+str(choice[2])+str(choice[3])+str(choice[4])+str(choice[5])
 print formatted
